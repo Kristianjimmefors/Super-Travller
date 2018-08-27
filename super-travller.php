e1fd057b1700f7947ce974c9a31d0e189419d371
Jonatan-Karrbrand <jonatan_96__@hotmail.com>
2018-08-27 12:53:43 +0200

    push

--- super-travller.php
+++ super-travller.php
@@ -12,8 +12,15 @@
   </head>
   <body>
 
-    <form class="" action="index.html" method="post">
-      <input type="checkbox" name="" value="">
+    <form class="" action="bag-check.php" method="post">
+      <input type="checkbox" name="Necessär" value="1">
+      <input type="checkbox" name="Sov säck" value="1">
+      <input type="checkbox" name="Bad madrass" value="5">
+      <input type="checkbox" name="Kylskåp" value="5">
+      <input type="checkbox" name="Mikro" value="3">
+      <input type="checkbox" name="Kol grill" value="4">
+
+      <input type="submit" name="submit" value="submit">
     </form>
 
   </body>
