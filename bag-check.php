05e6184dc62a56d9704f74e9586252ac8ab920d7
Kristian Jimmefors <kristian.jimmefors@gmail.com>
2018-08-27 14:00:56 +0200

    Add wheight calculateor

--- bag-check.php
+++ bag-check.php
@@ -9,17 +9,25 @@
     error_reporting(E_ALL);
 
 
-    class CalculateWeight{
+    class CalculateBag{
         public $items = [];
 
         function __construct(){
             foreach ($_SESSION as $key => $value) {
                 if ($key !== "submit") {
-                    $items[$key] = $value;
+                    $this->items[$key] = $value;
                 }
             }
         }
-
+        public function calculateWheight(){
+            if( array_sum($this->items) < 20 ){
+                echo "<p>Väskan är inte för tung</p>";
+                echo "väskan väger " . array_sum($this->items) . " kg";
+            } else{
+                echo "väskan väger för mycket";
+            }
+        }
     }
 
-$saker = new CalculateWeight();
+$saker = new CalculateBag();
+echo $saker->calculateWheight();
