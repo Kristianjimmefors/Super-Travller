a08bd499e2f26b52bfa3f2033cf934dba06626eb
Kristian Jimmefors <kristian.jimmefors@gmail.com>
2018-08-27 13:32:27 +0200

    Add a class

--- bag-check.php
+++ bag-check.php
@@ -2,3 +2,24 @@
 
     session_start();
     $_SESSION = $_POST;
+    
+    // Se alla fel under development.
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+
+    class CalculateWeight{
+        public $items = [];
+
+        function __construct(){
+            foreach ($_SESSION as $key => $value) {
+                if ($key !== "submit") {
+                    $items[$key] = $value;
+                }
+            }  
+        }
+        
+    }
+    
+$saker = new CalculateWeight();
