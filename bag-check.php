526d313943dd291f992fd77b77553e3da2655410
Jonatan-Karrbrand <jonatan_96__@hotmail.com>
2018-08-27 13:33:58 +0200

    a

--- bag-check.php
+++ bag-check.php
@@ -1,4 +1,30 @@
 <?php
 
-    session_start();
-    $_SESSION = $_POST;
+  session_start();
+  $_SESSION = $_POST;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Skriva ut saker
+
+// echo "Du har med";
+
+var_dump($_SESSION);
+foreach ($_SESSION as $key => $value) {
+  // echo $key;
+}
--- super-travller.php
+++ super-travller.php
@@ -15,22 +15,22 @@
     <form class="" action="bag-check.php" method="post">
       <label for="Necessär">Necessär</label>
       <input type="checkbox" name="Necessär" value="1">
-
+<br>
       <label for="Sov säck">Sov säck</label>
       <input type="checkbox" name="Sov säck" value="1">
-
+<br>
       <label for="Bad madrass">Bad madrass</label>
       <input type="checkbox" name="Bad madrass" value="5">
-
+<br>
       <label for="Kylskåp">Kylskåp</label>
       <input type="checkbox" name="Kylskåp" value="5">
-
+<br>
       <label for="Mikro">Mikro</label>
       <input type="checkbox" name="Mikro" value="3">
-
+<br>
       <label for="Kol grill">Kol grill</label>
       <input type="checkbox" name="Kol grill" value="4">
-
+<br>
       <input type="submit" name="submit" value="submit">
     </form>
 
