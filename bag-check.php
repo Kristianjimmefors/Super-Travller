f74e6e4bf75ebd46e7969f8cf9fbc9e641fa14a5
Kristian Jimmefors <kristian.jimmefors@gmail.com>
2018-08-27 15:01:11 +0200

    Change what calculateWheight returns

--- bag-check.php
+++ bag-check.php
@@ -4,14 +4,17 @@
     $_SESSION = $_POST;
 
     // Se alla fel under development.
+    /*
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+    */
 
 
     class CalculateBag{
         public $items = [];
 
+        //tar bort submit ifrån $items arrayen
         function __construct(){
             foreach ($_SESSION as $key => $value) {
                 if ($key !== "submit") {
@@ -19,26 +22,28 @@
                 }
             }
         }
-
+        //kollar om väskan väger för mycket
         public function calculateWheight(){
             if( array_sum($this->items) < 20 ){
-                echo "<p>Väskan är inte för tung</p>";
-                echo "väskan väger " . array_sum($this->items) . " kg";
+                echo "<p style = 'color: green;' >Väskan är inte för tung, det finns platts kvar</p>";
+                echo "<p>Väskan väger <span style = 'color: green;'>" . array_sum($this->items) . " kg</span> av 20kg</p>";
             } else{
-                echo "väskan väger för mycket";
+                echo "<p style = 'color: red;'>Väskan väger för mycket, något måste tas bort</p>";
+                echo "<p>Väskan väger <span style = 'color: red;'>" . array_sum($this->items) . " kg</span> av 20kg</p>";
             }
         }
+        //kollar vad som finns i väskan
         public function showBag(){
+            $stuff;
           foreach ($this->items as $key => $value) {
-            echo $key . ", ";
-
+             $stuff .= $key . ", ";
           }
+          return $stuff;
         }
 
     }
 
 $saker = new CalculateBag();
-echo $saker->calculateWheight();
-echo "Du har med dig: ";
-$saker->showBag();
+$saker->calculateWheight();
+echo "<p>Du har med dig: " . $saker->showBag() . "</p>";
 
